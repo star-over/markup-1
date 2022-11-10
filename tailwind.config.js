97d28c5455f598b08fba2264997aab26b6cd67b6
be1an <8497715@gmail.com>
2022-11-11 02:15:00 +0500

    fix: pixel-pixel to features

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,21 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
+    container: {
+      center: true,
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: '440px',
+        md: '728px',
+        lg: '936px',
+        xl: '1180px',
+      },
+    },
     extend: {
+      fontFamily: {
+        sans: 'Roboto, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+
+      },
       colors: {
         myRosePearl: "#EF2A82",
         myBunting: "#172755",
@@ -21,6 +35,12 @@ module.exports = {
         myBlueZodiac: "#0F1F4B",
         myAstronaut: "#2A407C",
         myWhite: "#FFFFFF",
+      },
+      backgroundImage: {
+        "my-top-lg": "url('/img/background-top-lg.png')",
+        "my-top-xs": "url('/img/background-top-xs.png')",
+        "my-bottom-lg": "url('/img/background-bottom-lg.png')",
+        "my-bottom-xs": "url('/img/background-bottom-xs.png')",
       },
     },
   },
